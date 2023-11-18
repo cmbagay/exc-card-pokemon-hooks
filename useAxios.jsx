4812db267bf82d321c83ee4d85de9795c6695989
cmbagay <126985167+cmbagay@users.noreply.github.com>
2023-11-18 11:40:56 -0800

    Create useAxios.jsx

---
+++ useAxios.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { v1 as uuid } from "uuid";
+
+
+
+function useAxios(url) {
+    const [cards, setCards] = useState([]);
+
+    const addCard = async () => {
+        const response = await axios.get(url);
+        setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    }
+
+    return [cards, addCard];
+}
+
+export default useAxios;
